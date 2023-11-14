5a81c3064c3d6f811d7f16834dc8fec9f0a3801a
yuko <yuriii.kovalyov@gmail.com>
2023-11-14 03:06:53 +0300

    Fixed words db

--- packages/backend/scripts/prepare_words.sh
+++ packages/backend/scripts/prepare_words.sh
@@ -1,2 +1,2 @@
 # russian_nouns.txt is quite a big file to include to the repository.
-cat russian_nouns.txt | node prepare_words.mjs | gzip --stdout > ./initdb/wordle.txt.gz
+cat russian_nouns.txt | gzip --stdout > ./initdb/wordle.txt.gz
